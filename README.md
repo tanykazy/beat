2327d19799778ac31a8ac49d214763ec46b7cdba
t-cool <hiroki802@hotmail.com>
2021-10-05 17:01:27 +0900

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# beat
+
+<img src="./images/icon/app-icon.png" width="100px">
+
+リズムに乗って意味順で英語を学ぶためのアプリです。
+
+## 構想・仕様
+
+[https://docs.google.com/presentation/d/1hSTLTyyPiBpRvptdsnTRtwwtCgyKsXLaIswN0ozImcU/edit?usp=sharing](https://docs.google.com/presentation/d/1hSTLTyyPiBpRvptdsnTRtwwtCgyKsXLaIswN0ozImcU/edit?usp=sharing)
+
+## ライセンス
+
+The GNU General Public License v3.0
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // 画面の向きを横向きに固定
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .then((_) => {runApp(Beating())});
+}
+
+// Beating: ビートを刻みながら音読する画面
+class Beating extends StatelessWidget {
+  const Beating({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Material App
+    return MaterialApp(
+      // Scaffold Widget
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Beat'),
+        ),
+        body: Center(child: MyStatelessWidget()),
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(label: Text('α')),
+        DataColumn(label: Text('だれが(なにが)')),
+        DataColumn(label: Text('する(です)')),
+        DataColumn(label: Text('だれ・なに')),
+      ],
+      rows: const <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            DataCell(Text('どこ')),
+            DataCell(Text('どう')),
+            DataCell(Text('いつ')),
+            DataCell(Text('なぜ')),
+          ],
+        ),
+      ],
+    );
+  }
+}
